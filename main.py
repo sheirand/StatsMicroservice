bf239bd780f4ec5e4c4896e341c6672bc294a85c
sheirand <eugene.osakovich@gmail.com>
2022-08-12 10:49:28 +0300

    [Pika+Rabbit] add: established connection to rabbitmq "innotter" queue

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+import asyncio
+from core.consumer import PikaClient
 from database.db import create_tables
 from endpoints.innotter import routes_stats
 
-app = FastAPI()
+
+class Statistic(FastAPI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pika_client = PikaClient(self.handle_incoming_messages)
+
+    @classmethod
+    def handle_incoming_messages(cls, body):
+        print("handle inc messages")
+
+
+app = Statistic()
 
 create_tables()
 
 app.include_router(routes_stats, prefix="/innotter")
+
+@app.on_event("startup")
+async def startup():
+    print("startup")
+    loop = asyncio.get_running_loop()
+    task = loop.create_task(app.pika_client.consume(loop))
+    await task
