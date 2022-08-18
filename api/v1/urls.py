ef0620ccc68d665e96bebd814ba25e829aaf96e2
sheirand <eugene.osakovich@gmail.com>
2022-08-18 15:23:11 +0300

    [JWT-auth] add: jwt-auth added to project

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from core.authentication import JWTAuthentication
 from models.innotter import Innotter
 from database.crud import CRUDManager
 
 router_innotter = APIRouter()
 
 
-@router_innotter.get("/get/{user_id}/{page_id}")
-def get_stats(user_id, page_id):
+@router_innotter.get("/get/{page_id}")
+def get_stats(page_id, user_id=Depends(JWTAuthentication())):
     """Temporary Template"""
-    return CRUDManager.get_stats(user_id=user_id, page_id=page_id)
+    return CRUDManager.get_stats(user_id=str(user_id), page_id=page_id)
---
+++ core/authentication.py
@@ -0,0 +1,53 @@
+from http.client import HTTPException
+from core.settings import JWT_SECRET_KEY
+import jwt
+from fastapi.security import HTTPBearer
+from fastapi import Request, HTTPException
+from fastapi.security.utils import get_authorization_scheme_param
+from database.db import dynamodb
+
+
+class JWTAuthentication(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTAuthentication, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        authorization: str = request.headers.get("Authorization")
+        scheme, credentials = get_authorization_scheme_param(authorization)
+        if not (authorization and scheme and credentials):
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=403, detail="Not authenticated"
+                )
+            else:
+                return None
+        return JWTAuthentication.has_access(credentials)
+
+    @staticmethod
+    def has_access(credentials):
+        """Function that is used to validate the token in the case that it requires it"""
+
+        token = credentials
+
+        try:
+            payload = jwt.decode(
+                token, key=JWT_SECRET_KEY, options={"verify_signature": False, "verify_aud": False, "verify_iss": False}
+            )
+
+            if not JWTAuthentication.check_user_id(str(payload["id"])):
+                raise HTTPException(
+                    status_code=403, detail="Not authenticated"
+                )
+            return str(payload["id"])
+
+        except (jwt.ExpiredSignatureError, jwt.DecodeError, jwt.InvalidTokenError) as error:
+            raise HTTPException(status_code=403, detail=f"{error}")
+
+    @staticmethod
+    def check_user_id(user_id: str):
+        table = dynamodb.Table("Innotter")
+        response = table.query(
+            KeyConditionExpression="user_id = :id",
+            ExpressionAttributeValues={":id": user_id}
+        )
+        return response["Items"]
